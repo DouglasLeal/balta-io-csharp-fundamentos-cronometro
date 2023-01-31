a908a66d5c170250f458f2821d9b78f99412e727
Douglas Leal <douglas.leal@live.com>
2023-01-30 23:11:13 -0300

    Criando o menu

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -1,9 +1,29 @@
-﻿Start();
+﻿Menu();
 
-static void Start()
+static void Menu()
 {
-    int time = 10;
+	Console.Clear();
+	Console.WriteLine("Cronômetro");
+	Console.WriteLine("---------------");
+	Console.WriteLine();
+	Console.WriteLine("Digite o tempo desejado para a contagem. (Digite 0 para sair.)");
 
+    Console.WriteLine();
+    Console.WriteLine("-------------------");
+    Console.WriteLine("|Exemplos:        |");
+    Console.WriteLine("|10s = 10 segundos|");
+    Console.WriteLine("|10m = 10 minutos |");
+    Console.WriteLine("-------------------");
+
+
+    Console.WriteLine();
+	Console.Write("Quanto tempo deseja? ");
+
+
+}
+
+static void Start(int time)
+{
 	while (time >= 0)
 	{
 		Console.Clear();
@@ -11,4 +31,10 @@ static void Start()
 		Thread.Sleep(1000);
 		time--;
 	}
+
+	Console.Clear();
+	Console.WriteLine("Contagem finalizada... retornando para o menu...");
+    Thread.Sleep(2000);
+
+	Menu();
 }
